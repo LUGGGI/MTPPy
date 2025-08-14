9f073b778764f909d84e134e3d51dc8b932eff74
Lukas Beck <lukas.beck@festo.com>
2025-08-14 18:14:11 +0200

    added type notation

--- src/mtppy/attribute.py
+++ src/mtppy/attribute.py
@@ -1,19 +1,24 @@
 import logging
 
+from collections.abc import Callable
+from typing import Any
+
+
 _logger = logging.getLogger(f"mtp.{__name__.split('.')[-1]}")
 
 
 class Attribute:
-    def __init__(self, name: str, data_type, init_value, sub_cb=None):
+    def __init__(self, name: str, data_type, init_value, sub_cb: Callable[[Any], None] = None):
         """
-        Atttribute represents an elementary object (attribute or parameter) of a data assembly. Depending on whether
+        Attribute represents an elementary object (attribute or parameter) of a data assembly. Depending on whether
         subscription callback is defined or not, it might be an monitored object or a static OPC UA node.
 
         Args:
             name (str): Attribute name.
             data_type (type): Attribute type.
             init_value (type): Initial value of the attribute.
-            sub_cb (callable, optional): Subscription callback if applied.
+            sub_cb (Callable[[Any], None], optional): Subscription callback. 
+                If defined, the attribute will be monitored.
         """
         self.name = name
         self.type = data_type
@@ -21,7 +26,7 @@ class Attribute:
         self.init_value = corrected_value
         self.value = corrected_value
         self.comm_obj = None
-        self.sub_cb = sub_cb
+        self.sub_cb: Callable[[Any], None] = sub_cb
 
     def set_value(self, value):
         """
@@ -73,12 +78,13 @@ class Attribute:
         """
         self.comm_obj = communication_object
 
-    def attach_subscription_callback(self, sub_cb):
+    def attach_subscription_callback(self, sub_cb: Callable[[Any], None]):
         """
         Attach a subscription callback to the attribute.
 
         Args:
-            sub_cb (callable): Subscription callback.
+            sub_cb (Callable[[Any], None]): 
+                Subscription callback function that will be called when the value changes.
         """
         self.sub_cb = sub_cb
 
